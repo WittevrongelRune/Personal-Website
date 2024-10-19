931cda77668b0671e2b37ec3ac9d3a6af8574e49
runewittevrongel <rune.wittevrongel@student.howest.be>
2024-10-19 23:31:32 +0200

    remove console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ function init() {
 
 function renderCopyrightYear() {
 const currentYear = new Date().getFullYear();
-console.log(currentYear);
 const $year = document.querySelector('#copyrightYear');
 $year.textContent = currentYear;
 }

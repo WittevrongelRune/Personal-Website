1c2c4d58c0f49bef16302db39f2890053e3ec499
runewittevrongel <rune.wittevrongel@student.howest.be>
2024-10-20 13:06:04 +0200

    made age responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ init();
 
 function init() {
     renderCopyrightYear();
+    renderAge();
 }
 
 function renderCopyrightYear() {
@@ -10,3 +11,23 @@ const currentYear = new Date().getFullYear();
 const $year = document.querySelector('#copyrightYear');
 $year.textContent = currentYear;
 }
+
+function renderAge() {
+    const birthDate = new Date(2005, 10, 8);
+    const currentDate = new Date();
+    
+    let age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    const monthDifference = currentDate.getMonth() - birthDate.getMonth();
+    const dayDifference = currentDate.getDate() - birthDate.getDate();
+    
+    if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+        age--;
+    }
+    const $age = document.querySelector('#age');
+    if ($age) {
+        $age.textContent = age;
+    }
+    console.log(age);
+}
+
